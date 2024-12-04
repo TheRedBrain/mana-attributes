f0cb894b3543f23317e76efaafc2f0fad9a51bb1
TheRedBrain <thetrueredbrain@gmail.com>
2024-12-04 13:43:26 +0100

    fixed mod menu not always getting the config

--- src/client/java/com/github/theredbrain/manaattributes/ManaAttributesClient.java
+++ src/client/java/com/github/theredbrain/manaattributes/ManaAttributesClient.java
@@ -7,10 +7,11 @@ import me.fzzyhmstrs.fzzy_config.api.RegisterType;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ManaAttributesClient implements ClientModInitializer {
-	public static ClientConfig CLIENT_CONFIG = ConfigApiJava.registerAndLoadConfig(ClientConfig::new, RegisterType.CLIENT);
+	public static ClientConfig CLIENT_CONFIG;
 
 	@Override
 	public void onInitializeClient() {
+		CLIENT_CONFIG = ConfigApiJava.registerAndLoadConfig(ClientConfig::new, RegisterType.CLIENT);
 		ClientEventsRegistry.initializeClientEvents();
 	}
 }
