a108d76422c8c2376e8e6daef3052933ebdad558
TheRedBrain <thetrueredbrain@gmail.com>
2025-10-04 21:27:22 +0200

    fixed first join mana amount

--- src/main/java/com/github/theredbrain/manaattributes/mixin/server/network/ServerPlayerEntityMixin.java
+++ src/main/java/com/github/theredbrain/manaattributes/mixin/server/network/ServerPlayerEntityMixin.java
@@ -4,15 +4,20 @@ import com.github.theredbrain.manaattributes.entity.ManaUsingEntity;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.ServerStatHandler;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity implements ManaUsingEntity {
+
+	@Shadow public abstract ServerStatHandler getStatHandler();
 
 	public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
 		super(world, pos, yaw, gameProfile);
@@ -21,6 +26,9 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity implements Ma
 	@Inject(method = "onSpawn", at = @At("TAIL"))
 	public void staminaattributes$onSpawn(CallbackInfo ci) {
 		this.manaattributes$setApplyOldMana(false);
+		if (this.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.LEAVE_GAME)) <= 0) {
+			this.manaattributes$setApplyMaxMana(true);
+		}
 	}
 
 }
